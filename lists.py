6ec3618ca1f1847bc821a12a4ba5e65cb849d32c
juancruzgon05 <162723271+juancruzgon05@users.noreply.github.com>
2024-05-17 10:40:45 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -35,11 +35,11 @@ def check_lists(list_to_compare1, list_to_compare2):
 
 
 def list_of_lists(list_of_lists_to_modify):
-    list_of_lists_to_modify[0][:2]
-    list_of_lists_to_modify[1][1:4]
-    list_of_lists_to_modify[2][2:]
+    lista1=list_of_lists_to_modify[0][:2]
+    lista2=list_of_lists_to_modify[1][1:4]
+    lista3=list_of_lists_to_modify[2][2:]
     
-    return list_of_lists_to_modify
+    return [lista1,lista2,lista3]
     
     
     
